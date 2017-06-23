9170a639f6990926ef54eb50ca914d851277d5c4
kumardippu <kumardippu@gmail.com>
2017-06-23 21:56:56 +0530

    first code

---
+++ application/views/register.php
@@ -0,0 +1,7 @@
+<?php //echo VIEWPATH;exit;echo $this->load->view('includes/header.php',TRUE);
+	include_once VIEWPATH."includes/header.php";
+?>
+<body id="page-top">
+</body>
+
+<?php include_once VIEWPATH."includes/footer.php"; ?>  
